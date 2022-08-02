d1dfcb7f5956adf94db6304525f5f358c96e8349
kutrala <110404182+kutrala@users.noreply.github.com>
2022-08-02 12:49:05 +0530

    hi

--- src/test/java/AutomationProject/AutomationProject/Order.java
+++ src/test/java/AutomationProject/AutomationProject/Order.java
@@ -1,13 +1,17 @@
 package AutomationProject.AutomationProject;
 
+import static org.testng.Assert.assertEquals;
+
 import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Order {
@@ -15,6 +19,7 @@ public class Order {
 public void Orders() {
 	System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
+	driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
 	driver.get("https://mobileworld.azurewebsites.net/");
 	driver.findElement(By.xpath("//a[contains(text(),'Support')]")).click();
 	driver.findElement(By.xpath("//a[contains(text(),'Order')]")).click();
@@ -29,13 +34,20 @@ public void Orders() {
     driver.findElement(By.id("inputPassword")).sendKeys("abcdefg");
 	driver.findElement(By.id("flexRadioDefault1")).click();
 	driver.findElement(By.xpath("//input[@placeholder='00000000000']")).sendKeys("9865098650");
-	driver.findElement(By.xpath(" address1")).sendKeys("1-123");
-	driver.findElement(By.xpath(" address2")).sendKeys("M.G road");
+	driver.findElement(By.id(" address1")).sendKeys("1-123");
+	driver.findElement(By.id("address2")).sendKeys("M.G road");
 	driver.findElement(By.id("inputCity")).sendKeys("Bengaluru");
 	WebElement state = driver.findElement(By.id("inputState"));	
 	Select sel=new Select(state);
 	sel.selectByVisibleText("Karnataka");
 	driver.findElement(By.id("inputZip")).sendKeys("560023");
-	driver.findElement(by.id)
+	driver.findElement(By.xpath("//*[@rel='apple']")).click();
+	driver.findElement(By.xpath("//*[@rel='samsung']")).click();
+	Assert.assertTrue(driver.findElement(By.xpath("//input[@rel='apple']")).isSelected());
+	WebElement iphone = driver.findElement(By.xpath("(//select[@class='multi_select'])[3]"));
+	Select seli=new Select(iphone);
+	seli.selectByVisibleText("Apple 13 ");
+	driver.findElement(By.xpath("//input[@placeholder='no of mobiles']")).click();
+	
 }
 }
