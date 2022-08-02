0ef42d1bc9a66540f771d33ddc8f9ffe72f7b624
kutrala <110404182+kutrala@users.noreply.github.com>
2022-08-02 18:10:49 +0530

    hi

--- src/test/java/AutomationProject/AutomationProject/DP.java
+++ src/test/java/AutomationProject/AutomationProject/DP.java
@@ -1,7 +1,37 @@
 package AutomationProject.AutomationProject;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-public class DataProvider {
-public static String[][] getData(){
+import org.apache.poi.xssf.usermodel.XSSFAnchor;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+public class DP {
+	
+@DataProvider
+public static String[][] getData() {
+	String[][] testdata=new String[2][4];
+	try {
+		FileInputStream file=new FileInputStream("./testdata/Book.xlsx");
+		XSSFWorkbook wb=new XSSFWorkbook(file);
+		XSSFSheet sheet = wb.getSheetAt(0);
+		int rowcount = sheet.getLastRowNum();
+		
+		  
+	} 
+	
+	catch (IOException e) {
+		// TODO: handle exception
+		e.printStackTrace();
+		
+	}
+	
+	catch(Exception f) {
+		f.printStackTrace();
+		
+	}
+	
 	return null;
 	
 }
