dd627fb80ebf1493cd5f77a377979168e13b086f
kutrala <110404182+kutrala@users.noreply.github.com>
2022-08-02 10:46:05 +0530

    hi

--- src/test/java/AutomationProject/AutomationProject/ContactUs.java
+++ src/test/java/AutomationProject/AutomationProject/ContactUs.java
@@ -1,5 +1,28 @@
 package AutomationProject.AutomationProject;
 
-public class ContactUs {
+import java.util.Set;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ContactUs {
+	
+@Test
+public void Contact() {
+	System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://mobileworld.azurewebsites.net/");
+	driver.findElement(By.xpath("//a[contains(text(),'Support')]")).click();
+	driver.findElement(By.xpath("(//a[contains(text(),'Contact Us')])[1]")).click();
+	Set<String> window = driver.getWindowHandles();
+	String parent = window.iterator().next();
+	String child = window.iterator().next();
+    driver.switchTo().window(child);
+    driver.findElement(By.name("name")).sendKeys("abcd");
+    driver.findElement(By.name("email")).sendKeys("abcd@gmail.com");
+    driver.findElement(By.name("phone")).sendKeys("9865098650");
+    
+}
 }
